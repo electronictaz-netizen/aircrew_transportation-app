2c39754db1f516591221f26cadac61f1f5218fae
electronictaz-netizen <electronictaz@gmail.com>
2026-01-26 12:33:09 -0500

    Fix Query syntax: use key object with partition key value directly

--- amplify/data/list-booking-requests-by-company.js
+++ amplify/data/list-booking-requests-by-company.js
@@ -2,17 +2,16 @@ import { util } from '@aws-appsync/utils';
 
 export function request(ctx) {
   const companyId = ctx.args.companyId;
-  const dynamoDbValue = util.dynamodb.toDynamoDB(companyId);
   
   // Use Query on the GSI instead of Scan with filter
   // GSI name: gsi-Company.bookingRequests
   // Partition key: companyId
+  // For Query operations, use the key value directly
   return {
     operation: 'Query',
     index: 'gsi-Company.bookingRequests',
-    keyConditionExpression: 'companyId = :cid',
-    expressionValues: {
-      ':cid': dynamoDbValue
+    key: {
+      companyId: util.dynamodb.toDynamoDB(companyId)
     },
     limit: 100,
   };
