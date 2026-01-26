8374d13fce66df1577366507253ce7b96d100cef
electronictaz-netizen <electronictaz@gmail.com>
2026-01-26 13:31:47 -0500

    Fix AppSync resolver syntax: use ddb.query() for GSI query

--- amplify/data/list-booking-requests-by-company.js
+++ amplify/data/list-booking-requests-by-company.js
@@ -1,27 +1,18 @@
-import { util } from '@aws-appsync/utils';
+import * as ddb from '@aws-appsync/utils/dynamodb';
 
 export function request(ctx) {
   const companyId = ctx.args.companyId;
   
-  // First, let's test if Scan works at all - scan without filter to get all items
-  // This will help us debug if the resolver is working
-  // We'll filter in the response function for now
-  return {
-    operation: 'Scan',
+  // Query the GSI gsi-Company.bookingRequests using companyId as the partition key
+  return ddb.query({
+    index: 'gsi-Company.bookingRequests',
+    key: {
+      companyId: companyId
+    },
     limit: 100,
-  };
+  });
 }
 
 export function response(ctx) {
-  const items = ctx.result.Items || [];
-  const companyId = ctx.args.companyId;
-  
-  // Filter items by companyId in JavaScript
-  // This will help us verify if the data is being returned
-  const filtered = items.filter(item => {
-    const itemCompanyId = util.dynamodb.fromDynamoDB(item.companyId);
-    return itemCompanyId === companyId;
-  });
-  
-  return filtered;
+  return ctx.result;
 }
